b677d4df5930d2de79441c754fb34557cc696e9d
Millicent-Cheptoi <90929904+Millicent-Cheptoi@users.noreply.github.com>
2021-12-17 23:16:16 +0300

    Add files via upload

---
+++ screencaster_133928.php
@@ -0,0 +1,29 @@
+<php
+function sanitizer ($[“£100”,’-1’,’200’,’300’,’-700’,678], $ split_string = ",", $array_type = false){
+
+$company = $ array[0];
+$amount = $ array[1];
+$value = -1, -700;
+
+foreach($amount as $array){
+
+$string =“archeNlandLimtded” ;
+$int_value = (int) $string;
+echo $int_value;
+
+}
+
+$str = str_replace('$', '', $str);{
+$str = trim($str, $);
+
+}
+
+if ($value < 0) {
+    $value = $value * -1;
+    
+}
+}
+$input1 = [[“archeNland Limtded”,[“£100”,’-1’,’200’,’300’,’-700’,678]];
+sanitizer($input1);
+
+?>
